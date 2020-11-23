296f17e33c4796aa755e662c127bb90cf8a1ceb8
Camille Schull <camille.schull@gmail.com>
2020-11-23 17:41:39 +0100

    Changes to models, building watch controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :watches do
+    resources :bookings, shallow: true, except: [:index]
+  end
+
+  # make method called dashboard
+  # need a view dashboard.html.erb
+  get "/dashboard", to: "bookings#dashboard"
+
 end
