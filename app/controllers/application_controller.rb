c4c668bf361b0f407e4ddd8aed0cccb763dbfdd6
Camille Schull <camille.schull@gmail.com>
2020-11-24 18:05:06 +0100

    changes to dashboard, sign in and stylistic elements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
+  def after_sign_out_path_for(resource)
+    watches_path
+  end
+
 end
