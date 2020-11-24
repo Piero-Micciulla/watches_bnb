043ef9f5882b4389b5e08a0c1663023c25286581
Camille Schull <camille.schull@gmail.com>
2020-11-24 10:51:06 +0100

    Bookings controller and views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,50 @@
 class BookingsController < ApplicationController
+   before_action :find_booking, only: [:show, :edit, :destroy]
 
+  def dashboard
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(strong_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @booking
+  end
+
+  def update
+    @booking
+    @booking.update(strong_params)
+    redirect_to dahsboard_path
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def find_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def strong_params
+    params.require(:watch).permit(:id, :description, :price)
+  end
 
 end
