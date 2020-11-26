15d42c91768d9eca8b13e8876caf8d379969fbd2
Camille Schull <camille.schull@gmail.com>
2020-11-26 20:48:58 +0100

    homepage and navbar edits

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,11 +3,20 @@ class Booking < ApplicationRecord
   belongs_to :user # not the owner, but the client
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validate :end_date_cannot_be_before_start_date
+  validate :date_mandatory
+  unless defined?(start_date) || defined?(end_date)
+    validate :end_date_cannot_be_before_start_date
+  end
 
   def end_date_cannot_be_before_start_date
     if end_date < start_date
       errors.add(:end_date, "can't be before the start date")
+    end
+  end
+
+  def date_mandatory
+    if start_date.nil? || end_date.nil?
+      errors.add(:start_date, "must enter date")
     end
   end
 end
